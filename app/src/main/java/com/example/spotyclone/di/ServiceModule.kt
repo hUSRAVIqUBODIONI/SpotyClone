d0536a4d3084c09b6ddf39434eac9c8ca3bfab3b
Шоимов Шоди <109214097+hUSRAVIqUBODIONI@users.noreply.github.com>
2025-12-09 21:03:41 +0300

    added hilt

---
+++ app/src/main/java/com/example/spotyclone/di/ServiceModule.kt
@@ -0,0 +1,72 @@
+package com.example.spotyclone.di
+
+import android.content.Context
+
+import androidx.annotation.OptIn
+import androidx.media3.common.AudioAttributes
+import androidx.media3.common.C.AUDIO_CONTENT_TYPE_MUSIC
+import androidx.media3.common.C.USAGE_MEDIA
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.exoplayer.ExoPlayer
+import com.example.spotyclone.data.remote.DataBase
+import com.example.spotyclone.repository.MusicRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ServiceComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.android.scopes.ServiceScoped
+import io.appwrite.Client
+import io.appwrite.services.Storage
+
+@Module
+@InstallIn(ServiceComponent::class)
+object ServiceModule {
+
+    @Provides
+    @ServiceScoped
+    fun provideMusicStorage( @ApplicationContext context: Context) : Storage{
+         val client : Client = Client(context)
+            .setEndpoint("https://fra.cloud.appwrite.io/v1")
+            .setProject("6925ad180027b51a9d19")
+
+         return Storage(client)
+    }
+
+    @Provides
+    @ServiceScoped
+    fun provideDataBase(
+        @ApplicationContext context: Context
+    ) : DataBase{
+        return DataBase(context)
+    }
+
+    @Provides
+    @ServiceScoped
+    fun provideMusicRepository(
+        dataBase: DataBase
+    ) : MusicRepository = MusicRepository(dataBase)
+
+
+
+    @Provides
+    @ServiceScoped
+    fun provideAudioAttributes()  = AudioAttributes.Builder()
+        .setContentType(AUDIO_CONTENT_TYPE_MUSIC)
+        .setUsage(USAGE_MEDIA)
+        .build()
+
+    @OptIn(UnstableApi::class)
+    @Provides
+    @ServiceScoped
+    fun provideExoPlayer(
+        @ApplicationContext context: Context,
+        audioAttributes: AudioAttributes
+    ): ExoPlayer {
+        return ExoPlayer.Builder(context).build().apply {
+            setAudioAttributes(audioAttributes, true)
+            setHandleAudioBecomingNoisy(true)
+        }
+    }
+
+}
