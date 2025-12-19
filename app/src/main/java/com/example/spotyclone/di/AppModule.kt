750c2505fbe86c7ab608c48ca9d0ba50048f7c08
Шоимов Шоди <109214097+hUSRAVIqUBODIONI@users.noreply.github.com>
2025-12-19 15:14:18 +0300

    AppModule room hilt

--- app/src/main/java/com/example/spotyclone/di/AppModule.kt
+++ app/src/main/java/com/example/spotyclone/di/AppModule.kt
@@ -1,13 +1,43 @@
 package com.example.spotyclone.di
 
+import android.content.Context
+import androidx.room.Room
+import com.example.spotyclone.data.db.SongDataBase
+import com.example.spotyclone.data.db.dao.SongDao
+import com.example.spotyclone.data.db.repository.RoomRepository
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
+    @Provides
+    @Singleton
+    fun provideRoomDataBase(
+        @ApplicationContext context: Context
+    ) : SongDataBase {
 
+        return Room.databaseBuilder(
+            context,
+            SongDataBase::class.java,
+            "room_song_db"
+        ).build()
+    }
+
+
+
+    @Provides
+    @Singleton
+    fun provideSongDao(db : SongDataBase) : SongDao = db.SongDao()
+
+
+    @Provides
+    @Singleton
+    fun provideRoomRepository(songDao: SongDao) : RoomRepository = RoomRepository(songDao)
 
 }
