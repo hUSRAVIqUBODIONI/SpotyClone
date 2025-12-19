e7b03fe3ab9e38c6427e6f1869305bc46f3f7da9
Шоимов Шоди <109214097+hUSRAVIqUBODIONI@users.noreply.github.com>
2025-12-19 15:14:47 +0300

    fixed bugs

--- app/src/main/java/com/example/spotyclone/repository/MusicRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.example.spotyclone.repository
-
-import com.example.spotyclone.data.remote.DataBase
-
-class MusicRepository(private val dataBase: DataBase) {
-    suspend fun getMusicList() = dataBase.getMusicList()
-}
